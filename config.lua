6feb937621155cfd996582ddc303c814e7bd1ab2
Azeroth <76072277+AzerothSixhten@users.noreply.github.com>
2022-07-19 13:08:12 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,9 +8,10 @@ Config.TrailerShop = {
         npccoords = vector3(-247.51, 6213.35, 31.94), 
         heading = 120.0, 
         ped = 'a_f_m_downtown_01', 
+        job = 'none',
         trailertobuy = {
-            {label = 'une remorque bateau', name = 'boattrailer', price = 250, coordsspawn = vector3(-251.8, 6223.12, 31.49), heading = 311.34, job = 'none'}, 
-            {label = 'une remorque voiture', name = 'trailersmall', price = 250, coordsspawn = vector3(-251.09, 6223.35, 32.49), heading =  311.34, job = 'none'}
+            {label = 'une remorque bateau', name = 'boattrailer', price = 250, coordsspawn = vector3(-251.8, 6223.12, 31.49), heading = 311.34}, 
+            {label = 'une remorque voiture', name = 'trailersmall', price = 250, coordsspawn = vector3(-251.09, 6223.35, 32.49), heading =  311.34}
         }
     },
 }
@@ -30,20 +31,20 @@ Config.VehicleCanTrail = {
     {name = 'WASTLNDR', class = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}},
 }
 
-Config.Lang = {
-    ["TrailerNotFind"] = 'Remorque non trouvée',
-    ["RampeAlreadySet"] = 'Une rampe est déjà posée',
-    ["NoVehicleSet"] = 'Aucun véhicule attachée trouvée',
-    ["CantSetThisType"] = 'Vous ne pouvez pas attacher ce genre de véhicule a cette remorque',
-    ["NotGoodJob"] = 'Vous ne faites pas partis du métier ciblée',
-    ["PressToOpen"] = 'Appuyez sur E pour ouvrir le menu',
-    ["YouBuy"] = 'Vous avez louer : ',
-    ["For"] = ' pour : ',
-    ["CantBuy"] = 'Tu ne peux pas acheter cela',
-    ["BuyFor"] = 'Louer pour : ',
-    ["Blip"] = 'Location de remorque',
-    ["TrailerNotFind"] = 'Remorque non trouvée',
-    ["NotInVehicle"] = 'Vous n\'êtes pas dans un véhicule'
+Config.Lang = {    
+    ["TrailerNotFind"] = 'Trailer not found',
+    ["RampeAlreadySet"] = 'A ramp is already attached',
+    ["NoVehicleSet"] = 'No vehicle attached found',
+    ["CantSetThisType"] = 'You cannot attach this type of vehicle to this trailer',
+    ["NotGoodJob"] = 'You are not in the targeted trade',
+    ["PressToOpen"] = 'Press E to open the menu',
+    ["YouBuy"] = 'You have rented: ',
+    ["For"] = 'For: ',
+    ["CantBuy"] = 'You can\'t buy that',
+    ["BuyFor"] = 'Rent for: ',
+    ["Blip"] = 'Rent a trailer',
+    ["TrailerNotFind"] = 'Trailer not found',
+    ["NotInVehicle"] = 'You are not in a vehicle'
 }
 
 Config.AttachCommand = 'attacherremorque'

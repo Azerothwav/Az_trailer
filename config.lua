e323281c396137719b2e7826da4b6040b14c9e84
Azeroth <76072277+Azerothwav@users.noreply.github.com>
2022-10-25 11:47:17 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -27,7 +27,6 @@ Config.VehicleCanTrail = {
     {name = 'DUBSTA3', class = {8}},
     {name = 'BOBCATXL', class = {8}},
     {name = 'BOATTRAILER', class = {14}},
-    {name = 'TRAILER', class = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 18, 19, 20}},
     {name = 'WASTLNDR', class = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}},
     {name = 'TRAILER', class = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}}
 }
